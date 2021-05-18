ad6dba196f631683df8824f5ea22eb0644ab50c9
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-05-18 15:30:51 +0200

    Updated "styklisten" code.
    Updated CalcCarport Class and Created CalcPart Class.

--- src/main/java/business/persistence/CalcCarport.java
+++ src/main/java/business/persistence/CalcCarport.java
@@ -1,7 +1,19 @@
 package business.persistence;
 
 public class CalcCarport {
+
+    // Method for calling the posts, beams and rafters
+
     public void calcPosts(int length, int width) {
 
     }
+
+    public void calcBeams(int length, int width) {
+
+    }
+
+    public void calcRafters(int length, int width) {
+
+    }
+
 }
---
+++ src/main/java/business/persistence/CalcPart.java
@@ -0,0 +1,19 @@
+package business.persistence;
+
+public class CalcPart {
+
+    public void calcPost() {
+        // Method for the actual calculations on how many post there has to be when the length is X.
+        // Maximum distance between 2 posts is. 310cm. 1 meter between the last post and the end of the beam.
+    }
+
+    public void calcBeam() {
+        //Method for the actual calculations on how many beams there has to be when the length of the roof is X.
+        // Infinite length of beam?
+    }
+
+    public void calcRafter() {
+        //Method for the actual calculations in how many rafters there has to be when the length of the beams is X.
+    }
+
+}
