4cc615c0c978a98734fcda301fe1cd15fd257fd6
Ermin Dzafic <71074936+ErminDZ@users.noreply.github.com>
2021-05-23 16:35:16 +0200

    done with svg

--- src/main/java/business/services/SVG.java
+++ src/main/java/business/services/SVG.java
@@ -20,12 +20,13 @@ public class SVG
 
     private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000000; fill: #ffffff\"/>";
 
-    private final String lineTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#000000;" +
-           " marker-start: url(#beginArrow);"+
-           " marker-end: url(#endArrow);\"/>";
+    private final String lineTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#000000;\"/>";
 
-    private final String markerTemplate = "<marker id=\"beginArrow\" markerWidth=\"12\" markerHeight=\"12\" refX=\"0\" refY=\"6\" orient=\"auto\"+ " +
-            "<path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000;\"/>";
+    private final String textvandretTemplate = " <text style=\"text-anchor: middle\" x=\"%d\" y=\"%d\">%s</text>";
+
+    private final String textlodretTemplate = "<text style=\"text-anchor: middle\" transform=\"translate(%d,%d) rotate(-90)\">%s</text>";
+
+    private final String stippledelineTemplate = "<line stroke-dasharray=\"5, 5\" x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />";
 
     public SVG(int x, int y, String viewBox, int width, int height) {
         this.x = x;
@@ -51,11 +52,20 @@ public class SVG
         svg.append(innerSVG.toString());
     }
 
-    public void addMarker()
+    public void addvandretText(int x,int y, String text)
     {
-
+        svg.append(String.format(textvandretTemplate,x,y,text));
     }
 
+    public void addlodretText(int x,int y, String text)
+    {
+        svg.append(String.format(textlodretTemplate,x,y,text));
+    }
+
+    public void addstippledeline(int x1, int y1, int x2, int y2)
+    {
+        svg.append(String.format(stippledelineTemplate,x1,y1,x2,y2));
+    }
 
     @Override
     public String toString() {
--- src/main/java/web/commands/showSVGCommand.java
+++ src/main/java/web/commands/showSVGCommand.java
@@ -17,13 +17,56 @@ public class showSVGCommand extends CommandUnprotectedPage
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException, SQLException
     {
-        SVG svgOuter = new SVG(0,0,"0 0 850 690", 100, 100);
-        // y-aksen/lodret
+        SVG svgOuter = new SVG(0,0,"0 0 850 720", 100, 100);
+
+        // y-aksen/lodret med text
         svgOuter.addLine(70,0,70,635);
-        // x-aksen/vandret
-        svgOuter.addLine(110,685,800,685);
-        // mål til Vandret spær
-        svgOuter.addLine(110,660,800,660);
+        svgOuter.addlodretText(65,330,"720cm");
+        // start pil til y-aksen
+        svgOuter.addLine(70,0,80,10);
+        svgOuter.addLine(70,0,60,10);
+        // slut pil til y-aksen
+        svgOuter.addLine(70,635,80,625);
+        svgOuter.addLine(70,635,60,625);
+        // x-aksen/vandret med text
+        svgOuter.addLine(105,685,800,685);
+        svgOuter.addvandretText(430,700,"620cm");
+        // start pil til x-aksen
+        svgOuter.addLine(105,685,115,695);
+        svgOuter.addLine(105,685,115,675);
+        // slut pil til x-aksen
+        svgOuter.addLine(800,685,790,695);
+        svgOuter.addLine(800,685,790,675);
+        // Vandret spær med mål
+        svgOuter.addLine(105,655,800,655);
+        svgOuter.addvandretText(135,675,"0,55");
+        svgOuter.addvandretText(190,675,"0,55");
+        svgOuter.addvandretText(240,675,"0,55");
+        svgOuter.addvandretText(295,675,"0,55");
+        svgOuter.addvandretText(350,675,"0,55");
+        svgOuter.addvandretText(400,675,"0,55");
+        svgOuter.addvandretText(455,675,"0,55");
+        svgOuter.addvandretText(505,675,"0,55");
+        svgOuter.addvandretText(560,675,"0,55");
+        svgOuter.addvandretText(615,675,"0,55");
+        svgOuter.addvandretText(665,675,"0,55");
+        svgOuter.addvandretText(720,675,"0,55");
+        svgOuter.addvandretText(775,675,"0,55");
+        //start-slut målinger
+        svgOuter.addLine(105,660,105,650);
+        svgOuter.addLine(160,660,160,650);
+        svgOuter.addLine(215,660,215,650);
+        svgOuter.addLine(270,660,270,650);
+        svgOuter.addLine(320,660,320,650);
+        svgOuter.addLine(375,660,375,650);
+        svgOuter.addLine(425,660,425,650);
+        svgOuter.addLine(480,660,480,650);
+        svgOuter.addLine(530,660,530,650);
+        svgOuter.addLine(585,660,585,650);
+        svgOuter.addLine(640,660,640,650);
+        svgOuter.addLine(693,660,693,650);
+        svgOuter.addLine(745,660,745,650);
+        svgOuter.addLine(800,660,800,650);
         //rem bund
         svgOuter.addLine(110,595,800,595);
         svgOuter.addLine(110,600,800,600);
@@ -42,9 +85,11 @@ public class showSVGCommand extends CommandUnprotectedPage
         svgOuter.addLine(110,0,800,0);
         // tag side bund
         svgOuter.addLine(110,637,800,637);
+        // kryds
+        svgOuter.addstippledeline(150,50,700,595);
+        svgOuter.addstippledeline(150,595,700,50);
 
         SVG svgInner = new SVG(0,0,"0 0 800 600", 100, 100);
-
         for (int x = 0; x < 14; x++)
         {
             svgInner.addRect(100 + 50 * x,0,600,4);
